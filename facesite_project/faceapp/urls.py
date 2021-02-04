9bd673f7daeeb68c10934c63303246c450bcbf7b
tamakhsunder <tamakhsunder@gmail.com>
2021-02-04 21:55:15 +0545

    extends commond used instead of include

--- facesite_project/faceapp/urls.py
+++ facesite_project/faceapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('chart/',views.chart,name='chart'),
+    path('table/',views.table,name='table'),
     # path('',views.home,name='home')
 ]
--- facesite_project/faceapp/views.py
+++ facesite_project/faceapp/views.py
@@ -5,10 +5,48 @@ from .models import Teacherdb
 # Create your views here.
 # def index(request):
 #     return HttpResponse('Hello world 2')
-def index(request):
-	teacherdbs=Teacherdb.objects
-	return render(request,'faceapp/index.html',{'teacherdbs':teacherdbs})
 
+
+
+
+# def database_collection():
+# 	teacherdbs=Teacherdb.objects
+# 	cursor = connection.cursor()
+# 	cursor.execute("SELECT COUNT(*) FROM faceapp_teacherdb")
+#     sql_totstaff = fetchone()
+#     print(connection.queries)
+# 	print('sunder')
+# 	print(sql_totstaff)
+# 	print('sunder')
+# 	return teacherdbs,sql_totstaff
+
+################
+
+def database_collection():
+	teacherdbs=Teacherdb.objects.all()
+	# sql_totstaff=Teacherdb.objects.raw("SELECT  COUNT(*) FROM faceapp_teacherdb")
+	sql_totstaff = Teacherdb.objects.all().count()
+	sql_present = Teacherdb.objects.filter(attendance="Present").count()
+	sql_absent = Teacherdb.objects.filter(attendance="Absent").count()
+	print('sunder')
+	print(sql_present)
+	print('sunder')
+	return teacherdbs,sql_totstaff,sql_present,sql_absent
+
+def index(request):
+	teacherdbs,sql_totstaff,sql_present,sql_absent=database_collection()
+	return render(request,'faceapp/index.html',{'teacherdbs':teacherdbs,'sql_totstaff':sql_totstaff,'sql_present':sql_present,'sql_absent':sql_absent})
+
+def chart(request):
+	return render(request,'faceapp/charts.html',{})
+
+def table(request):
+	teacherdbs,sql_totstaff,sql_present,sql_absent=database_collection()
+	return render(request,'faceapp/tables.html',{'teacherdbs':teacherdbs})
+
+
+#######
 
 # def home(request):
-# 	return render(request,'faceapp/index.html')
+# 	return render(request,'faceapp/index.html',{})
+
