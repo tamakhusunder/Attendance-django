1d9fe65faf65cb555ed7c1471cda09097c283c1d
tamakhusunder <51465716+tamakhusunder@users.noreply.github.com>
2021-03-09 10:57:09 +0545

    del it

--- facesite_project/faceapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import StaffInfo,AttendanceTb
-
-# Register your models here.
-admin.site.register(StaffInfo)
-admin.site.register(AttendanceTb)
--- facesite_project/faceapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FaceAppConfig(AppConfig):
-    name = 'faceapp'
