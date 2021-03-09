9c59e618be6db8800f13ab6d310d3aa07e30c950
tamakhusunder <51465716+tamakhusunder@users.noreply.github.com>
2021-03-09 10:34:34 +0545

    2model table added

--- facesite_project/faceapp/admin.py
+++ facesite_project/faceapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Teacherdb
+from .models import StaffInfo,AttendanceTb
 
 # Register your models here.
-admin.site.register(Teacherdb)
+admin.site.register(StaffInfo)
+admin.site.register(AttendanceTb)
